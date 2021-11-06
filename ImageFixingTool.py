60312e567b88f6830a06c47b564fd7972802cdd4
duncan <loukangzhi@outlook.com>
2021-11-06 11:17:56 +0800

    add some code to ImageFixingTool.py

--- ImageFixingTool.py
+++ ImageFixingTool.py
@@ -3,13 +3,14 @@ from astropy.io import fits
 from scipy import ndimage
 
 
-
 class ImageFixingTool:
 
-    def __init__(self):
-        return
+    def __init__(self, fits_img):
+        self.fits_img = fits_img
+        self.kernel = np.ones((3, 3))
 
-    def conv_mapping(x):
+
+    def conv_mapping(self, x):
         """
             When the fifth value (x[4]) of the filter array (the center of the window)
             is null, replace it with the mean of the surrounding values.
@@ -19,17 +20,12 @@ class ImageFixingTool:
         else:
             return x[4]
 
-    def fix_damaged_image(dat):
-        mask = np.ones((3, 3))
-        result = ndimage.generic_filter(dat, function=conv_mapping, footprint=mask, mode='constant', cval=np.NaN)
+    def fix_damaged_image(self, dat):
+        result = ndimage.generic_filter(dat, function=conv_mapping, footprint=self.kernel, mode='constant', cval=np.NaN)
 
         while np.isnan(result).any():
-            result = ndimage.generic_filter(result, function=conv_mapping, footprint=mask, mode='constant',
+            result = ndimage.generic_filter(result, function=conv_mapping, footprint=self.kernel, mode='constant',
                                             cval=np.NaN)
             row, col = np.where(np.isnan(result))
             print(f"Bad pixel num: {len(row)}")
-        print(f"Image:  {images} has been fixed completely, congratulations! ")
-
-
-
-
+        print(f"Image:  {self.fits_img} has been fixed completely, congratulations! ")
